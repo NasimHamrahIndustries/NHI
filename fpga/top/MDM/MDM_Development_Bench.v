17f9052eaaa99d92f2515fdb9b1be6f3d713b260
Reza Molaei <R.Molaei@mail.com>
2020-10-01 19:15:01 +0330

    MDM_Development_Bench.v(FPGA side) and MDM_Development_Bench.py(host pc side) were developed parallelly for elementary tests on MDM board.

--- fpga/top/MDM/MDM_Development_Bench.v
+++ fpga/top/MDM/MDM_Development_Bench.v
@@ -15,9 +15,9 @@ module MDM_Development_Bench #(
    // USB<->UART Pins
    output UART_TX, input UART_RX,
    // Intermediate Frequency Modulation
-   //output MULP, output MULN,
+   output MULP, output MULN,
    // LTC2312 Analog to Digital Converter
-   //input AFE_SDO, output AFE_SCK, output AFE_CONV,
+   input AFE_SDO, output AFE_SCK, output AFE_CONV,
    // LED Pin
    output LED
 );
@@ -25,11 +25,17 @@ module MDM_Development_Bench #(
    assign LED = ~UART_TX;
 
    wire rst;
+   reg cmd_rst = 1'b0;
    por_gen por_gen (
       .clk(CLOCK),
       .reset_out(rst)
    );
 
+   ////////////////////////////////
+   //
+   // UART Command Receiver
+   //
+   ////////////////////////////////
    // UART RX
    localparam UART_RX_SIZE = 1;
    localparam UART_clkdiv_rx= clk_freq/uart_rate;
@@ -39,16 +45,192 @@ module MDM_Development_Bench #(
    axis_uart_rx_wrapper #(
       .RX_SIZE(UART_RX_SIZE),
       .clkdiv_rx(UART_clkdiv_rx)
-   ) axis_uart_rx_wrapper (
-      .clk(CLOCK), .rst(rst),
-      // AXI Stream ports
-      .o_tdata(rx_tdata),
-      .o_tvalid(rx_tvalid),
-      .o_tready(rx_tready),
-      // Input RX port
-      .rx(UART_RX)
+   ) axis_uart_rx_wrapper (      //                                  |             |             |             |             |
+      .clk(CLOCK), .rst(rst),    //             ______  data         |   ______    |   ______    |   ______    |   ______    |
+      // AXI Stream ports        //            |      |==>===========O==|      |=>=O==|      |=>=O==|      |=>=O==|      |=>=O
+      .o_tdata(rx_tdata),        //            | UART | valid           | reg3 |      | reg2 |      | reg1 |      | reg0 |    
+      .o_tvalid(rx_tvalid),      // UART_RX -->| 125  |-->-----------O--|      |   O--|      |   O--|      |   O--|      |    
+      .o_tready(rx_tready),      //            | kb/s | ready        |  |______|   |  |______|   |  |______|   |  |______|    
+      // Input RX port           //            |______|--<-- 1'b1    O->-----------O->-----------O->-----------O              
+      .rx(UART_RX)               //                                                                                           
+   );
+   // Receiver registers
+   reg [7:0] reg3 = 8'b0;
+   reg [7:0] reg2 = 8'b0;
+   reg [7:0] reg1 = 8'b0;
+   reg [7:0] reg0 = 8'b0;
+   always @(posedge CLOCK)
+      if(rst) begin
+         reg3 <= 8'b0;
+         reg2 <= 8'b0;
+         reg1 <= 8'b0;
+         reg0 <= 8'b0;
+      end else if(rx_tvalid) begin
+         reg3 <= rx_tdata;
+         reg2 <= reg3;
+         reg1 <= reg2;
+         reg0 <= reg1;
+      end
+   assign rx_tready = 1'b1;
+
+   ////////////////////////////////
+   //
+   // Command Management
+   //
+   ////////////////////////////////
+   reg [1:0] command_state = 2'b00;
+   // command_state == 2'b00 : Waiting for command from host PC side and registering command and arguments
+   // command_state == 2'b01 : Doing command step1
+   // command_state == 2'b10 : Doing command step2
+   // command_state == 2'b11 : Illegal state
+   reg [7:0] arg1 = 8'b0;
+   reg [7:0] arg0 = 8'b0;
+   reg [7:0] cmd  = 8'b0;
+   reg [15:0] offset_value     = 16'd0;
+   reg        cal_offset       = 1'b0;
+   reg [7:0]  offset_mux       = 8'd0;
+   reg [15:0] threshold_value  = 16'd0;
+   reg [7:0]  capture_mux      = 8'd0;
+   reg        capture_fire     = 1'b0;
+   reg [7:0]  ask_value        = 8'd0;
+   reg [7:0]  ask_mux          = 8'd0;
+   always @(posedge CLOCK)
+      if(rst) begin
+            command_state   <= 2'b00;
+            cmd_rst         <= 1'b0;
+            offset_value    <= 16'd0;
+            cal_offset      <= 1'b0;
+            offset_mux      <= 8'd0;
+            threshold_value <= 16'd0;
+            capture_mux     <= 8'd0;
+            capture_fire    <= 1'b0;
+            ask_value       <= 8'd0;
+            ask_mux         <= 8'd0;
+         end
+      else
+         case (command_state)
+            2'b00 : begin
+                  if(rx_tvalid & rx_tdata==$unsigned(111) & reg0==$unsigned(110)) begin
+                     arg1 <= reg3;
+                     arg0 <= reg2;
+                     cmd  <= reg1;
+                     command_state <= 2'b01;
+                  end
+               end
+            2'b01 : begin
+                  case (cmd)
+                     8'h00 : cmd_rst         <= 1'b1;
+                     8'h01 : offset_value    <= {arg0, arg1};
+                     8'h02 : cal_offset      <= 1'b1;
+                     8'h03 : offset_mux      <= arg0;
+                     8'h04 : threshold_value <= {arg0, arg1};
+                     8'h05 : capture_mux     <= arg0;
+                     8'h06 : capture_fire    <= 1'b1;
+                     8'h07 : ask_value       <= arg0;
+                     8'h08 : ask_mux         <= arg0;
+                  endcase
+                  command_state <= 2'b10;
+               end
+            2'b10 : begin
+                  case (cmd)
+                     8'h00 : cmd_rst      <= 1'b0;
+                     8'h02 : cal_offset   <= 1'b0;
+                     8'h06 : capture_fire <= 1'b0;
+                  endcase
+                  command_state <= 2'b00;
+               end
+            default : command_state <= 2'b00;
+         endcase
+
+   ////////////////////////////////
+   //
+   // ADC Block
+   //
+   ////////////////////////////////
+   // LTC2312 ADC chip
+   wire [13:0] LTC2312_tdata;
+   wire LTC2312_tvalid;
+   LTC2312 #(
+      .WIDTH(14),
+      .clk_freq(clk_freq),
+      .sample_rate(LTC2312_sample_rate)
+   ) LTC2312 (
+      .clk(CLOCK), .rst(rst), .clear(1'b0),
+      .enable(1'b1),
+      // Output Stream ports
+      .o_tdata(LTC2312_tdata),
+      .o_tvalid(LTC2312_tvalid),
+      // SPI ports
+      .CONV(AFE_CONV), .SCK(AFE_SCK), .SDO(AFE_SDO)
    );
 
+   ////////////////////////////////
+   //
+   // DSP Blocks
+   //
+   ////////////////////////////////
+
+
+   ////////////////////////////////
+   //
+   // Fire Management
+   //
+   ////////////////////////////////
+   reg [1:0] fire_state = 2'b00;
+   // fire_state == 2'b00 : Waiting for command from host PC side
+   // fire_state == 2'b01 : Capturing a fram of samples
+   // fire_state == 2'b10 : Transmitting samples to PC
+   // fire_state == 2'b11 : Illegal state
+   // FIFO 16 bit to 8 bit, 2048 sample
+   reg [15:0] FIFO_DATA;
+   wire [7:0] FIFO_Q;
+   reg FIFO_WE = 1'b0;
+   wire FIFO_RE;
+   wire FIFO_FULL;
+   wire FIFO_EMPTY;
+   FIFOCore_2Bto1B_2048 FIFOCore_2Bto1B_2048 (
+      .DATA(FIFO_DATA),
+      .Q(FIFO_Q),
+      .WE(FIFO_WE),
+      .RE(FIFO_RE),
+      .CLK(CLOCK),
+      .FULL(FIFO_FULL),
+      .EMPTY(FIFO_EMPTY),
+      .RESET(rst)
+   );
+   // Capture MUX control
+   always @(*)
+      if(fire_state==2'b01)
+         case (capture_mux)
+            8'h00     : begin // adc
+                  FIFO_DATA <= {2'b00, LTC2312_tdata};
+                  FIFO_WE   <= LTC2312_tvalid;
+               end
+            /*8'h01     : begin // cutted_offset
+                  FIFO_DATA <= ;
+                  FIFO_WE   <= ;
+               end
+            8'h02     : begin // filtered
+                  FIFO_DATA <= ;
+                  FIFO_WE   <= ;
+               end
+            8'h03     : begin // auto
+                  FIFO_DATA <= ;
+                  FIFO_WE   <= ;
+               end
+            8'h04     : begin // manu
+                  FIFO_DATA <= ;
+                  FIFO_WE   <= ;
+               end*/
+            default : begin // Illegal
+                  FIFO_DATA <= 16'd0;
+                  FIFO_WE   <= 1'b1;
+               end
+         endcase
+      else begin
+         FIFO_DATA <= 16'd0;
+         FIFO_WE   <= 1'b0;
+      end
    // UART TX
    localparam UART_TX_SIZE = 1;
    localparam UART_clkdiv_tx = clk_freq/uart_rate;
@@ -67,10 +249,59 @@ module MDM_Development_Bench #(
       // Output TX port
       .tx(UART_TX)
    );
+   assign tx_tdata = FIFO_Q;
+   assign tx_tvalid = ( fire_state==2'b10 ? tx_tready : 1'b0 );
+   assign FIFO_RE   = ( fire_state==2'b10 ? tx_tready : 1'b0 );
+   // Fire control Unit
+   always @(posedge CLOCK)
+      if(rst)
+         fire_state <= 2'b00;
+      else
+         case (fire_state)
+            2'b00   : if(capture_fire) fire_state <= 2'b01;
+            2'b01   : if(FIFO_FULL)    fire_state <= 2'b10;
+            2'b10   : if(FIFO_EMPTY)   fire_state <= 2'b00;
+            default :                  fire_state <= 2'b00;
+         endcase
 
-   // development space
-   assign tx_tdata = rx_tdata;
-   assign tx_tvalid = rx_tvalid;
-   assign rx_tready = tx_tready;
+   ////////////////////////////////
+   //
+   // ASK Transmitter
+   //
+   ////////////////////////////////
+   // ASK TX
+   localparam ASK_TX_SIZE = 1;
+   localparam ASK_clkdiv_tx = clk_freq/ask_rate;
+   wire [7:0] ask_tx_tdata;
+   wire ask_tx_tvalid;
+   wire ask_tx_tready;
+   wire [1:0] ask_tx;
+   axis_ask_uart_tx_wrapper #(
+      .ask_core_type("simple"),
+      .ask_tx_length(2),
+      .TX_SIZE(ASK_TX_SIZE),
+      .clkdiv_tx(ASK_clkdiv_tx)
+   ) simple_axis_ask_uart_tx_wrapper (
+      .clk(CLOCK), .rst(rst),
+      // AXI Stream ports
+      .i_tdata(ask_tx_tdata),
+      .i_tvalid(ask_tx_tvalid),
+      .i_tready(ask_tx_tready),
+      // ASK output port
+      .ask_tx(ask_tx)
+   );
+   // ASK counter
+   reg [7:0] ask_counter = 8'd0;
+   always @(posedge CLOCK)
+      if(rst)
+         ask_counter <= 8'd0;
+      else if(ask_tx_tready & ask_mux==8'h02)
+         ask_counter <= ask_counter + 8'd1;
+   // ASK command control
+   assign ask_tx_tdata = ( ask_mux==8'h02 ? ask_counter : ask_value );
+   assign ask_tx_tvalid = ( ask_mux==8'h00 ? 1'b0 : 1'b1 );
+   // ASK output preparation
+   assign MULP = ( ask_tx==2'b01 ? 1'b1 : 1'b0 );
+   assign MULN = ( ask_tx==2'b11 ? 1'b1 : 1'b0 );
 
 endmodule // MDM_Development_Bench
--- host/MDM_Development_Bench.py
+++ host/MDM_Development_Bench.py
@@ -25,6 +25,10 @@ import matplotlib.pyplot as plt
 portName = '/dev/ttyUSB0'
 baudrateValue = 125000
 Vmax=2.048
+SampleRate = 200000  # sample per second
+SampleNumber=2048
+TimeStep=1/SampleRate
+ADCPrecision=Vmax/(2**14)
 MDM_Development_Bench_Arch="""
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Commands:                                                                  //        _________                 _                                        //
@@ -35,8 +39,8 @@ MDM_Development_Bench_Arch="""
 // set_threshold VALUE : e.g. set_threshold 1.25                             //         {0:0>8b} ===============|   |===|       |        ---' | | | ,---  //
 // capture manu/auto/filtered/cutted_offset/adc : Set mux stream direction   //                      ask value  |   |   | 5kb/s |             |_| |_|     //
 // capture fire : Capture 2048 samples from selected stream                  //                                 |   |   |       |--> MULN     10kHz       //
-// ask counter/value/off : Set mux stream direction                          //                       ask off --|  /    |_______|                         //
-// set_ask VALUE : e.g. set_ask 113  (It means  d113=b01110001)              //                                 |_/                                       //
+// set_ask VALUE : e.g. set_ask 113  (It means  d113=b01110001)              //                       ask off --|  /    |_______|                         //
+// ask counter/value/off : Set mux stream direction                          //                                 |_/                                       //
 // exit                                                                      //                              {ask_mux:^10}                                   //
 ///////////////////////////////////////////////////////////////////////////////                                                                           //
 //                                                                            set_threshold VALUE                                                         //
@@ -66,7 +70,7 @@ MDM_Development_Bench_Arch="""
 """
 '''
 //Instructions:
-// command_type  argument       Byte0 Byte1 Byte2
+// command_type  argument       cmd   arg0  arg1
 // reset                        h00   h00   h00
 // set_offset    fx.xx          h01   hXX   hXX
 // cal_offset                   h02   h00   h00
@@ -78,21 +82,21 @@ MDM_Development_Bench_Arch="""
 //               filtered       h05   h02   h00
 //               auto           h05   h03   h00
 //               manu           h05   h04   h00
-//               fire           h05   h05   h00
-// ask           off            h06   h00   h00
-//               value          h06   h01   h00
-//               counter        h06   h02   h00
+// capture       fire           h06   h00   h00
 // set_ask       dxxx           h07   hXX   h00
+// ask           off            h08   h00   h00
+//               value          h08   h01   h00
+//               counter        h08   h02   h00
 '''
 
 def main():
     print("In the Name of ALLAH")
+    offset_value=0.0
+    offset_mux='manu'
+    threshold_value=0.0
+    capture_mux='adc'
     ask_value=0
     ask_mux='off'
-    offset_value=0.0
-    threshold_value=0.0
-    offset_mux='manu'
-    capture_mux='adc'
     reset(' ')
     while 'true':
         print("MDM Development Bench:")
@@ -128,16 +132,16 @@ def main():
             val=capture(argument)
             if(val!='false' and val!='fire'):
                 capture_mux=val
-        elif(command_type=="ask"):
-            clear()
-            val=ask(argument)
-            if(val!='false'):
-                ask_mux=val
         elif(command_type=="set_ask"):
             clear()
             val=set_ask(argument)
             if(val!='false'):
                 ask_value=val
+        elif(command_type=="ask"):
+            clear()
+            val=ask(argument)
+            if(val!='false'):
+                ask_mux=val
         elif(command_type=="exit"):
             clear()
             break
@@ -152,10 +156,13 @@ def clear():
 
 def reset(argument):
     print("reset", argument)
+    serial_commander(portName, baudrateValue, int(0), int(0), int(0))
+    print("Done successfully!")
 
 def set_offset(argument):
     print("set_offset", argument)
     if(0.0<=float(argument) and float(argument)<=Vmax):
+        serial_commander(portName, baudrateValue, int(1), int(72), int(14))
         print("Done successfully!")
         return float(argument)
     else:
@@ -165,14 +172,17 @@ def set_offset(argument):
 
 def cal_offset(argument):
     print("cal_offset", argument)
+    serial_commander(portName, baudrateValue, int(2), int(0), int(0))
     print("Done successfully!")
 
 def offset(argument):
     print("offset", argument)
     if(argument=="manu"):
+        serial_commander(portName, baudrateValue, int(3), int(0), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="auto"):
+        serial_commander(portName, baudrateValue, int(3), int(1), int(0))
         print("Done successfully!")
         return argument
     else:
@@ -183,6 +193,7 @@ def offset(argument):
 def set_threshold(argument):
     print("set_threshold", argument)
     if(0.0<=float(argument) and float(argument)<=Vmax):
+        serial_commander(portName, baudrateValue, int(4), int(112), int(114))
         print("Done successfully!")
         return float(argument)
     else:
@@ -193,37 +204,58 @@ def set_threshold(argument):
 def capture(argument):
     print("capture", argument)
     if(argument=="adc"):
+        serial_commander(portName, baudrateValue, int(5), int(0), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="cutted_offset"):
+        serial_commander(portName, baudrateValue, int(5), int(1), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="filtered"):
+        serial_commander(portName, baudrateValue, int(5), int(2), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="auto"):
+        serial_commander(portName, baudrateValue, int(5), int(3), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="manu"):
+        serial_commander(portName, baudrateValue, int(5), int(4), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="fire"):
+        serial_commander(portName, baudrateValue, int(6), int(0), int(0))
+        fire_ploter(portName, baudrateValue, SampleNumber, ADCPrecision)
         print("Done successfully!")
-        return 'fire'
+        return argument
     else:
         print("Command argument should be : manu, auto, filtered, cutted_offset, adc or fire !")
+        print("Irregular command!")
+        return 'false'
+
+def set_ask(argument):
+    print("set_ask", argument)
+    if(0<=int(argument) and int(argument)<=255):
+        serial_commander(portName, baudrateValue, int(7), int(argument), int(0))
+        print("Done successfully!")
+        return int(argument)
+    else:
+        print("Command argument should be a integer number in range: 0 <= argument and argument <= 255 !")
         print("Irregular command!")
         return 'false'
 
 def ask(argument):
     print("ask", argument)
     if(argument=="counter"):
+        serial_commander(portName, baudrateValue, int(8), int(2), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="value"):
+        serial_commander(portName, baudrateValue, int(8), int(1), int(0))
         print("Done successfully!")
         return argument
     elif(argument=="off"):
+        serial_commander(portName, baudrateValue, int(8), int(0), int(0))
         print("Done successfully!")
         return argument
     else:
@@ -231,15 +263,76 @@ def ask(argument):
         print("Irregular command!")
         return 'false'
 
-def set_ask(argument):
-    print("set_ask", argument)
-    if(0<=int(argument) and int(argument)<=255):
-        print("Done successfully!")
-        return int(argument)
-    else:
-        print("Command argument should be a integer number in range: 0 <= argument and argument <= 255 !")
-        print("Irregular command!")
-        return 'false'
+def serial_commander(portName, baudrateValue, cmd, arg0, arg1):
+    ser=serial.Serial()
+    ser.port = portName
+    ser.baudrate = baudrateValue
+    ser.open()
+    # start Byte sign
+    startByte=110
+    si=(startByte).to_bytes(1, byteorder='big')
+    ser.write(si)
+    # cmd
+    si=(cmd).to_bytes(1, byteorder='big')
+    ser.write(si)
+    # arg0
+    si=(arg0).to_bytes(1, byteorder='big')
+    ser.write(si)
+    # arg1
+    si=(arg1).to_bytes(1, byteorder='big')
+    ser.write(si)
+    # stop Byte sign
+    stopByte=111
+    si=(stopByte).to_bytes(1, byteorder='big')
+    ser.write(si)
+    '''# for loopback test
+    i=0
+    ii=[0, 0, 0, 0, 0]
+    while i<5:
+        ri = ser.read()
+        ii[i] = int.from_bytes(ri, byteorder='big')
+        i=i+1
+    print("stop:", ii[4], " ,arg1:", ii[3], " ,arg0:", ii[2], " ,cmd:", ii[1], " ,start:", ii[0])
+    #'''
+    ser.close()
+
+def fire_ploter(port, baudrate, SampleNumber, Precision):
+    ser=serial.Serial()
+    ser.port = port
+    ser.baudrate = baudrate
+    ser.open()
+    # Start receve
+    i=0
+    ri = ser.read()
+    ii0 = int.from_bytes(ri, byteorder='big')
+    ri = ser.read()
+    ii1 = int.from_bytes(ri, byteorder='big')
+    sample_received=[Precision*float(ii1+ii0*(2**8))]
+    i=i+1
+    while ( i < SampleNumber ) :
+        ri = ser.read()
+        ii0 = int.from_bytes(ri, byteorder='big')
+        ri = ser.read()
+        ii1 = int.from_bytes(ri, byteorder='big')
+        sample_received.append(Precision*float(ii1+ii0*(2**8)))
+        i=i+1
+    ser.close()
+    # Plot
+    i=0
+    TimeSteps=[float(0)]
+    freTest=float(SampleRate/51)
+    SinusoidalWaveTest=[numpy.sin(2.0*numpy.pi*freTest*TimeSteps[0])]
+    while (i<SampleNumber-1):
+        TimeSteps.append(TimeSteps[i]+TimeStep)
+        i=i+1
+        SinusoidalWaveTest.append(numpy.sin(2.0*numpy.pi*freTest*TimeSteps[i]))
+        freTest=freTest*1.001
+    plt.plot(TimeSteps, SinusoidalWaveTest)
+    plt.plot(TimeSteps, SinusoidalWaveTest, 'go')
+    plt.ylabel('ADC Values (Volt)')
+    plt.xlabel('Time (Second)')
+    plt.grid()
+    plt.show()
 
 if __name__ == '__main__':
     main()
